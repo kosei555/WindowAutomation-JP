9c466dffafbd68a10caa57d5d6b61e91abfd7757
kosei555 <keto.skyday@gmail.com>
2026-05-10 14:24:40 +0900

    基盤作成

---
+++ src/基盤/CommandNodeClass.ps1
@@ -0,0 +1,48 @@
+class CommandNode{
+    [hashtable] $m_commandChildren
+    [String] $m_commandNodeName
+    CommandNode([String]$commandNodeName){
+        $this.m_commandChildren=@{}
+        $this.m_commandNodeName=$commandNodeName
+    }
+    #[String]GetChildrenName(){
+    #}
+    [CommandNode]GetChild([String] $commandNodeName){
+        return $this.m_commandChildren[$commandNodeName]
+    }
+    RegisterCommand([String] $commandNodeName){
+        $this.m_commandChildren[$commandNodeName]=[CommandFactory]::Create($commandNodeName)
+    }
+
+}
+class CommandFactory{
+    static [CommandNode] Create ([String]$commandNodeName){
+        switch($commandNodeName){
+            "open"{
+                return [CommandOpen]::new()
+            }
+            "close"{
+                return [CommandClose]::new()
+            }
+        }
+        return [CommandNode]::new()
+    }
+}
+class CommandOpen : CommandNode{
+    CommandOpen() : base ("open"){
+    }
+    Exec([String]$folderPath){
+        $targetFiles=Get-ChildItem -Path $folderPath -File | Where-Object{$_.Name -Like "*.lnk"}
+        ForEach ($file In $targetFiles){
+            start-Proces $file.FullName
+        }
+
+    }
+}
+class CommandClose : CommandNode{
+        CommandClose() : base ("close"){
+    }
+    Exec([String]$folderName){
+        
+    }
+}
