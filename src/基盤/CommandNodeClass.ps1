117910ea2a9f83c703b94aa4d517e35a78d901da
kosei555 <keto.skyday@gmail.com>
2026-05-10 16:12:11 +0900

    CommandInit削除

--- src/基盤/CommandNodeClass.ps1
+++ src/基盤/CommandNodeClass.ps1
@@ -58,38 +58,3 @@ class CommandClose : CommandNode{
     }
 }
 
-function CommandInit(){#将来的にはJSONファイル読み込ませる
-    $cmd = [CommandNode]::new([CommndNodeCommonConfig]::ROOT_CMD_NAME)
-    $cmd.RegisterCommand("files")
-    $cmd.GetChild("files").RegisterCommand("open")
-    return $cmd
-}
-
-function GetCommandNode([CommandNode]$cmdRoot,[String]$command){#誤ったコマンド打ち込んだ時の処理を追加する必要性あり
-    $queue = New-Object System.Collections.Queue
-    $strBuf=""
-    $cmdNode=$null
-    $ea = [ExecArgs]::new()
-
-    For ($i=0;$i -lt $command.Length;$i=$i+1){
-        If($command[$i] -eq " "){
-            $queue.Enqueue($strBuf)
-            $strBuf=""
-        }
-        Else {
-            $strBuf=$strBuf + $command[$i]
-        }
-    }
-    $queue.Enqueue($strBuf)
-    while($queue.Count -gt 0){
-        $queContent=$queue.Dequeue()
-        if($queContent -eq [CommndNodeCommonConfig]::ROOT_CMD_NAME){
-            $cmdNode=$cmdRoot
-
-        }
-        else{
-            $cmdNode=$cmdNode.GetChild($queContent)
-        }
-    }
-    return $cmdNode
-}
